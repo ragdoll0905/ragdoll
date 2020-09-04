fc97e8c05c120e6e5baafd267367ca478a60b875
ragdoll <gaon0905@naver.com>
2020-09-04 18:05:16 +0900

    modify !

--- index.js
+++ index.js
@@ -109,12 +109,12 @@ client.on('message', (message) => {
     if(message.channel.type != 'dm' && checkPermission(message)) return
 
     var clearLine = message.content.slice('!청소 '.length);
-    var isNum = !isNaN(clearLine)
+    var isNum = isNaN(clearLine)
 
     if(isNum && (clearLine <= 0 || 100 < clearLine)) {
       message.channel.send("1부터 100까지의 숫자만 입력해주세요.")
       return;
-    } else if(!isNum) { // c @나긋해 3
+    } else if(isNum) { // c @나긋해 3
       if(message.content.split('<@').length == 2) {
         if(isNaN(message.content.split(' ')[2])) return;
 
